1f2c97181b6690ee85548ca94f43b5732e3def04
kotrabakoff <71779665+kotrabakoff@users.noreply.github.com>
2023-02-12 22:55:35 -0500

    Add files via upload

---
+++ Unit 0/Randomization.py
@@ -0,0 +1,18 @@
+import numpy as np
+def randomization(n):
+    """
+    Arg:
+      n - an integer
+    Returns:
+      A - a randomly-generated nx1 Numpy array.
+    """
+    list = [int(n), 1]
+    output = np.random.random(list)
+    return np.array(output)
+    raise NotImplementedError
+
+print(randomization(3))
+
+#%%
+
+#%%
